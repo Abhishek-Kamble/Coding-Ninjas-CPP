64a6539c41dcc6620c29fb6e8fccc55843256ce6
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-07-20 23:30:02 +0530

    updated staircase

--- Recursion 2/staircase.cpp
+++ Recursion 2/staircase.cpp
@@ -39,12 +39,9 @@ int staircase(int n){
     else
     {
         x=staircase(n-1);
-    //if(n>2)
         y=staircase(n-2);
-    //if(n>3)
         z=staircase(n-3);
-
-    return x+y+z;
+        return x+y+z;
     }
 }
 
