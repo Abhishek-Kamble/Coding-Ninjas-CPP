f7506c01c972e494213179d636884ed8cd090897
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-09-03 00:54:22 +0530

    Update QueueUsingArrays.h

--- Stacks and Queue/QueueUsingArrays.h
+++ Stacks and Queue/QueueUsingArrays.h
@@ -1,56 +1,92 @@
 template <typename T>
-class QueueUsingArrays{
+
+class QueueUsingArray
+{
     T *data;
-    int size;
-    int firstIndex;
-    int capacity;
     int nextIndex;
+    int firstIndex;
+    int size;
+    int capacity;
 
 public:
-    QueueUsingArrays(int s){
+    QueueUsingArray(int s)
+    {
         data = new T[s];
+        nextIndex = 0;
+        firstIndex = -1;
         size = 0;
         capacity = s;
-        firstIndex = -1;
-        nextIndex = 0;
     }
 
-    int getSize(){
+    int getSize()
+    {
         return size;
     }
 
-    int isEmpty(){
+    bool isEmpty()
+    {
         return size == 0;
     }
 
-    T front(){
-        return data[firstIndex];
-    }
-
-    void enqueue(T element){
-        if(size == capacity){
-            cout << "Queue is full!" << endl;
-            return;
+    // insert element
+    void enqueue(T element)
+    {
+        if (size == capacity)
+        {
+            T *newData = new T[2 * capacity];
+            int j = 0;
+            for (int i = firstIndex; i < capacity; i++)
+            {
+                newData[j] = data[i];
+                j++;
+            }
+            for (int i = 0; i < firstIndex; i++)
+            {
+                newData[j] = data[i];
+                j++;
+            }
+            delete[] data;
+            data = newData;
+            firstIndex = 0;
+            nextIndex = capacity;
+            capacity *= 2;
+            //cout << "Queue Full ! " << endl;
+            // return;
         }
-
         data[nextIndex] = element;
         nextIndex = (nextIndex + 1) % capacity;
-        if(firstIndex == -1){
+        if (firstIndex == -1)
+        {
             firstIndex = 0;
         }
         size++;
     }
 
-    T dequeue(){
-        if(isEmpty()){
-            cout << "Queue is empty!" << endl;
+    T front()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty ! " << endl;
             return 0;
         }
+        return data[firstIndex];
+    }
 
+    T dequeue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty ! " << endl;
+            return 0;
+        }
         T ans = data[firstIndex];
         firstIndex = (firstIndex + 1) % capacity;
         size--;
+        if (size == 0)
+        {
+            firstIndex = -1;
+            nextIndex = 0;
+        }
         return ans;
     }
-
 };
